7fee90b2d06f094c6769da17a8d40ed630a0abdc
MrBukas <bukanovns@gmail.com>
2021-12-24 17:49:56 +0300

    fix

--- backend/database_queries.py
+++ backend/database_queries.py
@@ -5,9 +5,9 @@ def get_taken_rooms_query(year, month):
     select 
     room_id room,
     date_format(start_date, '%d.%m.%Y') start, 
-    date_format(leave_date, '%d.%m.%Y') leave from accomodations
+    date_format(leave_date, '%d.%m.%Y') leaved from accomodations
 where start_date > '{y}-{m}-1' and leave_date < '{y}-{m}-31'
-order by room_id, start_date;
+order by room_id, start_date
     """
     return query
 
